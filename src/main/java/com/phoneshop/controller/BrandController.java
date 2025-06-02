2c9a18c36a50c720bc0ea663d56f1b443d0dd06c
plongrotha <Mrr.rothabetta31@gmail.com>
2025-06-02 15:30:30 +0700

    feature/ModelEnitiy: merge code from another branch and it work

--- src/main/java/com/phoneshop/controller/BrandController.java
+++ src/main/java/com/phoneshop/controller/BrandController.java
@@ -1,33 +1,20 @@
 package com.phoneshop.controller;
 
-import java.time.LocalTime;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import com.phoneshop.exception.NotFoundException;
-import com.phoneshop.mapper.BrandMapperImpl;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.phoneshop.dto.BrandDTO;
 import com.phoneshop.mapper.BrandMapper;
 import com.phoneshop.model.entity.Brand;
 import com.phoneshop.model.response.ApiResponse;
 import com.phoneshop.service.BrandService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/brands")
@@ -36,7 +23,6 @@ public class BrandController {
 
 	// inject used constructor
 	private final BrandService brandService;
-	private final BrandMapperImpl brandMapperImpl;
 
 	@Operation(summary = "Create brand", description = "Create a brand to database")
 	@PostMapping
